ef70ca6da6d15545bf76508e9942568c7a852517
Imre Szanyi <imre_szanyi@epam.com>
2015-11-26 16:22:00 +0100

    init calc function

--- src/view/main/main.directive.js
+++ src/view/main/main.directive.js
@@ -8,10 +8,14 @@
 		}
 
 		function mainController($scope, calcService) {
+
+			$scope.outs = [ 0.5, 1.5, 2.5, 3.5, 4.5, 5.5 ];
+
 			$scope.less_odds = [ 11.5, 3.6, 1.9, 1.35, 1.12, 1.03 ];
-			$scope.less_bet = [ 0, 0, 0, 0, 0, 0];
+			$scope.less_bet = [ 0, 0, 0, 0, 0, 0 ];
+
 			$scope.more_odds = [ 1.03, 1.22, 1.7, 2.7, 4.65, 10.5 ];
-			$scope.more_bet = [ 0, 0, 0, 0, 0, 0];
+			$scope.more_bet = [ 0, 0, 0, 0, 0, 0 ];
 
 			$scope.betText = "bt";
 			$scope.oddsText = "od";
--- src/view/main/main.service.js
+++ src/view/main/main.service.js
@@ -6,9 +6,45 @@
             calculate: calculate
 		}
 
+        var myScope;
+
         function calculate($scope) {
-            
-            return $scope.more_odds5 * 2;
+            myScope = $scope;
+            $scope.result_less_bet = [0, 0, 0, 0, 0, 0 ]
+
+            //iterate these
+            var la = [1, 1, 1, 1, 1, 1];
+            var ma = [1, 1, 1, 1, 1, 1];
+            return doCalc(la, ma, 3);
+
         }
+
+        function ff(la) {
+            return la.length;
+        }
+
+        function doCalc(betLessArr, betMoreArr, gnum) {
+            //betArr = [ 12 x b ]
+            // less
+
+            var lw = 0;
+            var mw = 0;
+
+            for(var i = 0; i < betLessArr.length; i++) {
+                if ( gnum < myScope.outs[i] ) {
+                    lw = lw + (betLessArr[i] * myScope.less_odds[i] - betLessArr[i]);
+                }
+            }
+
+            for(var i = 0; i < betMoreArr.length; i++) {
+                if ( gnum > myScope.outs[i] ) {
+                    mw = mw + (betMoreArr[i] * myScope.more_odds[i] - betMoreArr[i]);
+                }
+            }
+
+            return (mw + lw) - 6;
+        }
+
+
     });
 })(angular);
